936abc3ed15e34e38f76a798036ac85d70f6f686
Sabin789 <87514484+Sabin789@users.noreply.github.com>
2022-11-28 15:31:21 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+let firstName="Sabin"
+let middleName=" Marian"
+let lastName=" Andrei"
+ 
+
+let fullName= firstName+middleName+lastName
+console.log("Hello, my name is ", fullName)
+
+age=21
+currentYear=2022
+
+birthYear= currentYear-age
+console.log("My year of birth:",birthYear)
+
+let spaghetti= 400
+let guanciale =250
+let oneEggYolk= 18
+let pecorinoRomano= 50
+let rawBlackPepper= 4
+
+let eggAndPecorino=pecorinoRomano+= oneEggYolk *6
+console.log("Adding the egg yolks to the cheese:",eggAndPecorino)
+ let roast=rawBlackPepper+=eggAndPecorino
+ console.log("Adding black pepper to the mixture:",roast)
+ let pastaGuanciale= spaghetti+=guanciale
+ console.log("Adding guanicale to the pasta:",pastaGuanciale)
+ let carbonara= pastaGuanciale+roast
+ console.log("Carbonara:",carbonara)
+
+
